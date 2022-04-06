242db1b1305f640445c8e05cc1058c134c3a3031
Narong-Bunyon <90994369+Narong-Bunyon@users.noreply.github.com>
2022-04-06 22:14:40 +0700

    Delete mixs directory

--- mixs/mix.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once '../models/post.php';
-require_once '../templates/header.php';
-require_once '../views/post_form.php';
-require_once '../views/comment.php';
-require_once '../templates/footer.php';
-?>
